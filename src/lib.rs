4012722ca4b28291794b085d9ca6abb80cec4a5f
Simone <simone.giacomelli@gmail.com>
2023-01-12 21:11:28 +0000

    checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod rpc;
 pub mod proxy;
+pub mod webserver;
---
+++ src/webserver.rs
@@ -0,0 +1,12 @@
+pub struct HttpRequest {
+    pub method: String,
+    pub content: String,
+    pub content_type: String,
+}
+
+
+
+pub struct HttpResponse {
+    pub content: String,
+    pub content_type: String,
+}
