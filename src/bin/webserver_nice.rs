fda5585f02b65efbacef84d1cc14925597a9b521
Simone <simone.giacomelli@gmail.com>
2023-01-26 21:02:58 +0000

    checkpoint

--- src/bin/webserver_nice.rs
+++ src/bin/webserver_nice.rs
@@ -79,30 +79,7 @@ mod tests {
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
-    #[tokio::test]
-    async fn test_wait() {
-        tokio::spawn(async {
-            webserver_start(|req, ctx| -> HttpResponse {
-                println!(" hello !");
-                let content = format!("<h1>from a fn, url: {}</h1>", req.url);
 
-                HttpResponse {
-                    content,
-                    content_type: "text/html".to_string(),
-                    status: 404,
-                    headers: HashMap::new(),
-                }
-            }).await.unwrap();
-        });
-
-        wait_webserver_responsive("http://127.0.0.1:4444").await;
-        tokio::time::sleep(Duration::new(1, 0)).await;
-        reqwest::get("http://127.0.0.1:1337").await.unwrap().text().await;
-        tokio::time::sleep(Duration::new(1, 0)).await;
-        // std::thread::sleep(Duration::new(5, 0));
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
 
     async fn async_function(msg: &str) {
         println!("{}", msg);
--- src/webserver/wait_webserver.rs
+++ src/webserver/wait_webserver.rs
@@ -1,17 +1,54 @@
+use std::collections::HashMap;
 use std::time::Duration;
+use crate::find_port::find_port;
+use crate::webserver::HttpResponse;
+use crate::webserver::tokio_server::webserver_start;
 
 pub async fn wait_webserver_responsive(url: &str) {
-    for _ in 0..300 {
+    wait_webserver_responsive_times(url, 300).await.unwrap();
+}
+
+pub async fn wait_webserver_responsive_times(url: &str, times: usize) -> Result<(), String> {
+    for _ in 0..times {
         let res = reqwest::get(url).await;
-        println!("{:?}", res);
         match res {
-            Ok(ok) => { return; }
+            Ok(ok) => { return Ok(()); }
             Err(err) => {
-                println!("is_connect={}", err.is_connect());
+                // println!("is_connect={}", err.is_connect());
             }
         }
-
         tokio::time::sleep(Duration::from_millis(1)).await;
     }
-    panic!("timeout waiting for {}", url);
+    Err(format!("timeout waiting for {}", url))
+}
+
+#[tokio::test]
+async fn test_wait_nothing() {
+    let free_port = find_port().unwrap();
+    let url = format!("http://127.0.0.1:{}", free_port);
+    let result = wait_webserver_responsive_times(&url, 10).await;
+    if let Ok(_) = result { panic!("Ci si aspettava un timeout") }
+}
+
+#[tokio::test]
+async fn test_wait() {
+    let port = find_port();
+    tokio::spawn(async {
+        webserver_start(|req, ctx| -> HttpResponse {
+            HttpResponse {
+                content: "no content".to_string(),
+                content_type: "text/html".to_string(),
+                status: 404,
+                headers: HashMap::new(),
+            }
+        }).await.unwrap();
+    });
+
+    wait_webserver_responsive("http://127.0.0.1:4444").await;
+    tokio::time::sleep(Duration::new(1, 0)).await;
+    reqwest::get("http://127.0.0.1:1337").await.unwrap().text().await;
+    tokio::time::sleep(Duration::new(1, 0)).await;
+    // std::thread::sleep(Duration::new(5, 0));
+    let result = 2 + 2;
+    assert_eq!(result, 4);
 }
