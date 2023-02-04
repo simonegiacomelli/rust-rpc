876e268e4a21e00807ec91b087fe2bc75105a7ad
Simone <simone.giacomelli@gmail.com>
2023-02-04 17:01:02 +0000

    checkpoint

--- src/rpc/conversions.rs
+++ src/rpc/conversions.rs
@@ -2,6 +2,12 @@ use serde::Serialize;
 use serde::de::DeserializeOwned;
 use std::fmt::Debug;
 
+pub fn rpc_req_to_str<Req>(req: &Req) -> String
+    where Req: ?Sized + Serialize + DeserializeOwned + Debug {
+    let req_json = serde_json::to_string(req).unwrap();
+    req_json
+}
+
 pub fn rpc_req_from_str<Req>(payload: &str) -> Req
     where Req: ?Sized + Serialize + DeserializeOwned + Debug {
     let req: Req = serde_json::from_str(payload).unwrap();
@@ -12,12 +18,6 @@ pub fn rpc_res_to_str<Res>(res: &Res) -> String
     where Res: ?Sized + Serialize + DeserializeOwned + Debug {
     let res_json = serde_json::to_string(&res).unwrap();
     res_json
-}
-
-pub fn rpc_req_to_str<Req>(req: &Req) -> String
-    where Req: ?Sized + Serialize + DeserializeOwned + Debug {
-    let req_json = serde_json::to_string(req).unwrap();
-    req_json
 }
 
 pub fn rpc_res_from_str<Res>(res_payload: &String) -> Res
