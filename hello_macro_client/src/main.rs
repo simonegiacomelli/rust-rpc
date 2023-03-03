610b5efce64000ea4751563eca3fe75af7131065
Simone <simone.giacomelli@gmail.com>
2023-03-03 20:47:54 +0000

    macro ok 2

--- hello_macro_client/src/main.rs
+++ hello_macro_client/src/main.rs
@@ -4,6 +4,10 @@ use hello_macro_derive::HelloMacro;
 #[derive(HelloMacro)]
 struct Pancakes;
 
+#[derive(HelloMacro)]
+struct Muffin;
+
 fn main() {
     Pancakes::hello_macro();
+    Muffin::hello_macro();
 }
