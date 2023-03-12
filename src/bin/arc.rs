3e77486f1bbbdbecc4e40ce7553b65c7d3df2de2
Simone <simone.giacomelli@gmail.com>
2023-03-12 17:57:59 +0000

    checkpoint

---
+++ src/bin/arc.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+use tokio::count;
+
+fn main() {
+    let counter2 = Arc::new(HashMap::new());
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+}
---
+++ src/bin/box2.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let a = Box::new(Box::new(Box::new(5)));
+    let b = plus_one(***a) + 37;
+    println!("{}", b)
+}
+
+fn plus_one(&n: i32) -> i32 { n + 1 }
