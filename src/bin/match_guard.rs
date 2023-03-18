782cd3b2ffc6cdab161300c90a13cf8289382d5b
Simone <simone.giacomelli@gmail.com>
2023-03-18 17:28:45 +0000

    checkpoint

--- src/bin/match_guard.rs
+++ src/bin/match_guard.rs
@@ -14,4 +14,12 @@ fn main() {
         None => (),
         _ => (),
     }
+
+    let x = 4;
+    let y = false;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"),
+        _ => println!("no"),
+    }
 }
