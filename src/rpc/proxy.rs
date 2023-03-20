5404d2b66dd03cde865c89a3eb42e16d4295ee95
Simone <simone.giacomelli@gmail.com>
2023-03-20 21:41:10 +0000

    checkpoint

--- src/rpc/proxy.rs
+++ src/rpc/proxy.rs
@@ -3,10 +3,15 @@ use std::fmt::Debug;
 use async_trait::async_trait;
 use serde::de::DeserializeOwned;
 use serde::Serialize;
+use trait_set::trait_set;
 
 use crate::rpc;
 use crate::rpc::{conversions, get_handler_key};
 use crate::rpc::handlers::{Payload, Request};
+
+trait_set! {
+    pub trait ReqResBound = Serialize + DeserializeOwned + Debug ;
+}
 
 pub struct Proxy {
     transport: Box<dyn Transport>,
@@ -24,10 +29,8 @@ impl Proxy {
             transport: Box::new(transport)
         }
     }
-    pub async fn send<Req, Res>(&self, req: &Req) -> Result<Res,String>
+    pub async fn send<Req: ReqResBound, Res: ReqResBound>(&self, req: &Req) -> Result<Res, String>
         where Req: Request<Res>,
-              Req: ?Sized + Serialize + DeserializeOwned + Debug,
-              Res: ?Sized + Serialize + DeserializeOwned + Debug,
     {
         let handler_key = get_handler_key::<Req>();
         let req_json = conversions::rpc_req_to_str(req);
@@ -38,4 +41,12 @@ impl Proxy {
         let res = conversions::rpc_res_from_str(&res_payload);
         res
     }
+
+    // fn request_to_payload<Req>(&self, req: &Req) -> String {
+    //     let handler_key = get_handler_key::<Req>();
+    //     let req_json = conversions::rpc_req_to_str(req);
+    //     let payload = Payload { handler_key: handler_key.as_str(), json: req_json.as_str() };
+    //     let req_payload = payload.to_string();
+    //     req_payload
+    // }
 }
