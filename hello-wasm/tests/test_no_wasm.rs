36d04e4484992c50bea141773221d952e5f07182
Simone <simone.giacomelli@gmail.com>
2023-04-01 17:22:56 +0100

    checkpoint

--- hello-wasm/tests/test_no_wasm.rs
+++ hello-wasm/tests/test_no_wasm.rs
@@ -4,11 +4,6 @@ mod test {
     use rpc_api::rpc_api_add;
 
     #[test]
-    fn test_no_wasm() {
-        assert_eq!(get_string(), "Hello, hello-wasm!")
-    }
-
-    #[test]
     fn it_works() {
         let result = rpc_api_add(2, 2);
         assert_eq!(result, 4);
