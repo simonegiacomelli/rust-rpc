7c0e4055cdc9c482e0929447d2cf06d69b609e0d
Simone <simone.giacomelli@gmail.com>
2023-04-04 21:32:24 +0100

    cleaning

--- migrations/001_anag_aree.sql
+++
@@ -1,5 +0,0 @@
-create table anag_aree
-(
-    id_azienda int,
-    cod_area   varchar(100)
-);
--- migrations/002_anag_aree.sql
+++
@@ -1,6 +0,0 @@
-drop table anag_aree;
-create table anag_aree
-(
-    id_azienda int          not null,
-    cod_area   varchar(100) not null
-);
